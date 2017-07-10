3ad74058ef7302e49f5b613392fe4064a1c62010
Sudeshna Ghosh <sudeshnacsesit@gmail.com>
2017-07-10 17:04:47 +0530

    first push

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Home page</title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="container-fullwidth logo-container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 logo-col">
+				<img src="assets/img/onlinelogomaker-070817-1218-1826.png" alt="">
+				<h3 style="margin-top: 0px; color: #FFFFFF;">Study Hub</h3>
+				<h4 style="margin-top: -7px; color: #FFFFFF;">Play Hard Study Hard</h4>
+			</div>
+		</div>
+	</div>
+
+	<nav class="navbar navbar-default navbar-container" role="navigation">
+		<div class="container-fluid ">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav nav-text-color">
+					<li><a href="#" class="nav-link">Home</a></li>
+					<li><a href="#" class="nav-link">About Me</a></li>
+					<li><a href="#" class="nav-link">Contact Me</a></li>
+				</ul>
+			
+				
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+
+	<section class="heroimage">
+		<div class="container img-container">
+			<div class="row img-row">
+				<div class="col-md-6 col-md-offset-3 img-content">
+					<h1 style="font-weight: 700; font-size: 36px;">
+						Lorem ipsum dolor sit amet
+					</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<a class="btn btn-info btn-bgcolor" href="#popup1">BOOK NOW</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<div class="container sub-container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+				<a href="#"><img src="assets/img/image-sub.jpg" class="img-responsive" alt="Image">
+				<h1>Computer Network</h1></a>
+			</div>
+			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+				<img src="assets/img/ArtificialIntelligence.jpg" class="img-responsive" alt="Image">
+				<h1>Artificial Intelligence</h1>
+			</div>
+			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+				<img src="assets/img/code.jpg" class="img-responsive" alt="Image">
+				<h1>Compiler Design</h1>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+	<!-- Content end -->
+	<hr>
+
+	<div id="popup1" class="overlay">
+		<div class="popup">
+			<h2>Here i am</h2>
+			<a class="close" href="#">×</a>
+			<div class="content">
+				Thanks for pop me out of that button, but now i'm done so you can close this window.
+			</div>
+		</div>
+	</div>
+
+</body>
+	<script src="assets/js/jquery.js" type="text/javascript" charset="utf-8"></script>
+	<script src="assets/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
+</script>
+</html>
