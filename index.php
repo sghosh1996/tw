a2ec35d7496fafce0756bc78c452f0f64da5443d
Sudeshna Ghosh <sudeshnacsesit@gmail.com>
2017-07-10 17:06:37 +0530

    book now 2

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<a class="btn btn-info btn-bgcolor" href="#popup1">BOOK NOW</a>
+					<a class="btn btn-info btn-bgcolor" href="#popup1">BOOK NOW 2</a>
 				</div>
 			</div>
 		</div>
